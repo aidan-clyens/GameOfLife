d5c9c6b53d8d2c9ae525a0ca15b781123d38e802
Aidan Clyens <aidanc@rogers.com>
2020-01-13 22:22:53 -0500

    Initial package commit and pygame setup

---
+++ conway/game.py
@@ -0,0 +1,33 @@
+import pygame
+
+BLACK = pygame.color.Color(0, 0, 0)
+
+class Game:
+    def __init__(self, screen_size):
+        pygame.init()
+
+        self._screen_size = screen_size
+
+        self._screen = pygame.display.set_mode(screen_size)
+        self._clock = pygame.time.Clock()
+
+        self._running = True
+
+    def draw(self):
+        self._screen.fill(BLACK)
+        pygame.display.flip()
+    
+    def poll_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+    
+    def run(self):
+        while self._running:
+            self.poll_events()
+            self.draw()
+
+            self._clock.tick(60)
+    
+    def quit(self):
+        pygame.quit()
---
+++ main.py
@@ -0,0 +1,11 @@
+from conway.game import Game
+
+
+def main():
+    game = Game([800, 600])
+    game.run()
+    game.quit()
+
+
+if __name__ == '__main__':
+    main()
